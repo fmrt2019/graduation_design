ed2e87104d699ebb9b6aa02b17edc6bccacdd0db
FMRT19-dragon <107900676+FMRT19-dragon@users.noreply.github.com>
2023-04-15 08:47:56 +0800

    Update datapath.v
    实例化EX_stage，WB_stage，RNR_stage，RNR_RR_reg。完善了RNR_stage，RNR_RR_reg接口信号表

--- datapath.v
+++ datapath.v
@@ -5,43 +5,72 @@ Create date:2023.04.14 11:18
 Description:overall datapath.
 *******************************************************************************/
 module datapath ();
-/*************************************************************EX_stage***************************************/
+/************************************************************RNR_stage****************************************/
+    //input
+	wire	[164:0]		ifmt1_rn,		//第一条指令来自译码的控制信息
+	wire	[164:0]		ifmt2_rn,		//第二条指令来自译码的控制信息
+    //output
+	//1
+	wire	[164:0]		nuifmt1rn,		//第一条输入到RNR_RR_reg的控制信号
+	wire	rd1_enrn,				//第一条指令的目的寄存器写使能信号(提交判断)
+	wire	[4:0]		rd1lrn,			//第一条指令的目的寄存器逻辑号(提交判断)
+	//2
+	wire	[164:0]		nuifmt2rn,		//第二条输入到RNR_RR_reg的控制信号
+	wire	rd2_enrn,				//第二条指令的目的寄存器写使能信号(提交判断)
+	wire	[4:0]		rd2lrn			//第二条指令的目的寄存器逻辑号(提交判断)
+/*************************************************************************************************************/
+/************************************************************RNR_RR_reg***************************************/
+	//input		[164:0]	nuifmt1rn,		//第一条来自译码但是没有用到输入到RNR_RR_reg的控制信号
+	wire	[5:0]		rs1prn,			//第一条指令的rs寄存器的物理号
+	wire	[5:0]		rt1prn,			//第一条指令的rt寄存器的物理号
+	wire	[5:0]		rd1prn,			//第一条指令的目的寄存器物理号
+	//input		[164:0]	nuifmt2rn,		//第二条来自译码但是没有用到输入到RNR_RR_reg的控制信号
+	wire	[5:0]		rs2prn,			//第二条指令的rs寄存器的物理号
+	wire	[5:0]		rt2prn,			//第二条指令的rt寄存器的物理号
+	wire	[5:0]		rd2prn,			//第二条指令的目的寄存器物理号
+						
+	wire	[164:0]		ifmt1rr,		//第一条来自译码但是没有用到输出到RR阶段的控制信号
+	wire	[5:0]		rs1prr,			//第一条指令的rs寄存器的物理号
+	wire	[5:0]		rt1prr,			//第一条指令的rt寄存器的物理号
+	wire	[5:0]		rd1prr,			//第一条指令的目的寄存器物理号
+
+	wire	[164:0]		ifmt2rr,		//第二条来自译码但是没有用到输出到RR阶段的控制信号
+	wire	[5:0]		rs2prr,			//第二条指令的rs寄存器的物理号
+	wire	[5:0]		rt2prr,			//第二条指令的rt寄存器的物理号
+	wire	[5:0]		rd2prr			//第二条指令的目的寄存器物理号
+/*************************************************************************************************************/
+/*************************************************************RR_stage****************************************/
+	
+/*************************************************************************************************************/
+/*************************************************************EX_stage****************************************/
     //input
 	//alu
-	wire	[31:0]	aluae,alube;				//执行阶段alu的两个操作数
-	wire	[2:0]	alucontrole;				//执行阶段alu的控制信号
-	wire	[5:0]	alurde;					//执行阶段alu中的指令的目的寄存器物理号
-	wire	aluene;						//执行阶段alu中的指令是否有效
+	wire	[31:0]	aluae,alube;			//执行阶段alu的两个操作数
+	wire	[2:0]	alucontrole;			//执行阶段alu的控制信号
 	//sfu
-	wire	[31:0]	sfuae,sfube;				//执行阶段sfu的两个操作数
-	wire	[1:0]	sfucontrole;				//执行阶段sfu的控制信号
-	wire	[5:0]	sfurde;					//执行阶段sfu中的指令的目的寄存器物理号
-	wire	sfuene;						//执行阶段sfu中的指令是否有效
+	wire	[31:0]	sfuae,sfube;			//执行阶段sfu的两个操作数
+	wire	[1:0]	sfucontrole;			//执行阶段sfu的控制信号
 	//bru
-	wire	[31:0]	bruae,brube;				//执行阶段bru的两个操作数
-	wire	[2:0]	brucontrole;				//执行阶段bru的控制信号
-	wire	pdce;						//执行阶段bru中分支指令的预测方向
-	wire	[31:0]	paddre;					//执行阶段bru中分支指令的预测地址
-	wire	[5:0]	brurde;					//执行阶段bru中的指令的目的寄存器物理号
-	wire	bruene;						//执行阶段bru中的指令是否有效
+	wire	[31:0]	bruae,brube;			//执行阶段bru的两个操作数
+	wire	[2:0]	brucontrole;			//执行阶段bru的控制信号
+	wire	pdce;					//执行阶段bru中分支指令的预测方向
+	wire	[31:0]	paddre;				//执行阶段bru中分支指令的预测地址
 	//agu
-	wire	[31:0]	aguae,agube;				//执行阶段agu的两个操作数
-	wire	[3:0]	agucontrole;				//执行阶段agu的控制信号
-	wire	[5:0]	agurde;					//执行阶段agu中的指令的目的寄存器物理号
-	wire	aguene;						//执行阶段agu中的指令是否有效
+	wire	[31:0]	aguae,agube;			//执行阶段agu的两个操作数
+	wire	[3:0]	agucontrole;			//执行阶段agu的控制信号
     //out
 	//alu
-	wire	[31:0]	aluoute;				//执行阶段alu的输出
-	wire	overflowe;					//执行阶段alu是否溢出
+	wire	[31:0]	aluoute;			//执行阶段alu的输出
+	wire	overflowe;				//执行阶段alu是否溢出
 	//sfu
-	wire	[31:0]	sfuoute;				//执行阶段sfu的输出
+	wire	[31:0]	sfuoute;			//执行阶段sfu的输出
 	//bru
-	wire	prighte;					//执行阶段bru中分支指令是否预测正确
-	wire	b_typee;					//执行阶段bru中分支指令的类型（1代表B类型）
-	wire	rdce;						//执行阶段bru中分支指令的真正方向
-	wire	[31:0]	raddre;					//执行阶段bru中分支指令的真正地址
+	wire	prighte;				//执行阶段bru中分支指令是否预测正确
+	wire	b_typee;				//执行阶段bru中分支指令的类型（1代表B类型）
+	wire	rdce;					//执行阶段bru中分支指令的真正方向
+	wire	[31:0]	raddre;				//执行阶段bru中分支指令的真正地址
   	//agu
-	wire	[31:0]	aguoute;				//执行阶段agu的输出
+	wire	[31:0]	aguoute;			//执行阶段agu的输出
 /**************************************************************************************************************/
 	
   decode_stage decode (
@@ -83,18 +112,23 @@ module datapath ();
 		 rs1_rnr_2,rs2_rnr_2,rd_rnr_2,ld_st_num_rnr_2,imm_rnr_2,next_pc_rnr_2,rs_bru_pc_rnr_2,
 		 is_split_rnr_2,instr_valid_rnr_2}
 	);
-/*
+	
+	RNR_stage	rnr_stage	(ifmt1_rn,ifmt2_rn,nuifmt1rn,rd1_enrn,rd1lrn,nuifmt2rn,rd2_enrn,rd2lrn);
+	
+	RNR_RR_reg	rnr_rr_reg	(clk,rst,stall,recover,nuifmt1rn,rs1prn,rt1prn,rd1prn,nuifmt2rn,rs2prn,rt2prn,rd2prn,ifmt1rr,rs1prr,rt1prr,rd1prr,
+					 ifmt2rr,rs2prr,rt2prr,rd2prr);
+
 	dispath_stage dispath (
 		
 
 
 	)
+	EX_stage	ex_stage	(alu_ae,alu_be,alucontrole,sfu_ae,sfu_be,sfucontrole,bru_ae,bru_be,brucontrole,pdce,paddre,agu_ae,agu_be,
+					agucontrole,aluoute,overflowe,sfuoute,prighte,b_typee,rdce,raddre,aguoute);
 	
-
-
-
-
-
+	WB_stage	wb_stage	(prightw,b_typew,rdcw,rd1ps,rd2ps,rd1_enrn,rd1lrn,rd2_enrn,rd2lrn,rd_en1w,memwen1w,valid1w,rd1lw,rd1pw,rd_en2w,
+					memwen2w,valid2w,rd2lw,rd2pw,br_right_en,raddren,srat_1en1,arfen1,write1,srat_1en2,arfen2,write2);
+	
 
 
 
